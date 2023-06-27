a493228868712f50eb137c7f3673b56faae5a63d
jgalamba <jfgalamba@mail>
2023-06-27 11:37:25 +0100

    Added new documents related to GitHub and TFTP

--- src/client.py
+++ src/client.py
@@ -15,9 +15,9 @@ def main():
     print("TFTPy - Cliente de TFTP (em desenvolvimento)")
     print("Usage:")
     print("""\
-    $ python3 client.py [-p serv_port] server
-    $ python3 client.py get [-p serv_port] server remote_file [local_file] 
-    $ python3 client.py put [-p serv_port] server local_file [remote_file]
+$ python3 client.py [-p serv_port] server
+$ python3 client.py get [-p serv_port] server remote_file [local_file] 
+$ python3 client.py put [-p serv_port] server local_file [remote_file]
     """)
 #:
 
